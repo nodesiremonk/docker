b35d76903c2a76cebbfbe599377e0016b4ac05ce
NoDesireMonk <chinglish@gmail.com>
2019-04-23 11:28:35 -0500

    Update vdocker.sh

--- vdocker.sh
+++ vdocker.sh
@@ -458,7 +458,7 @@ function install_certbot {
 
 function install_docker {
 	apt update && apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
-	curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+	curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 	add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 	apt update && apt -y install docker-ce docker-ce-cli containerd.io
 	# might need to reboot here
