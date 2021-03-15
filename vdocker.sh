eb5a2cc27cc42cbef3f5ae5b3947e4c43629cef5
NoDesireMonk <chinglish@gmail.com>
2021-03-15 18:08:51 -0500

    Update vdocker.sh

--- vdocker.sh
+++ vdocker.sh
@@ -457,13 +457,13 @@ function install_certbot {
 }
 
 function install_docker {
-	apt update && apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+	apt update && apt -y install apt-transport-https ca-certificates curl gnupg lsb-release
 	curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 	add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 	apt update && apt -y install docker-ce docker-ce-cli containerd.io
 	# might need to reboot here
-	# change 1.26.0 to latest version
-	curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+	# change 1.28.5 to latest version
+	curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 	chmod +x /usr/local/bin/docker-compose
 }
 
