94961dcf99b8f44da686dd0d50d2822d39bf234d
NoDesireMonk <chinglish@gmail.com>
2021-08-21 15:57:17 -0500

    Update vdocker.sh

--- vdocker.sh
+++ vdocker.sh
@@ -470,7 +470,7 @@ function install_docker {
 	# old method ==> add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 	apt update && apt -y install docker-ce docker-ce-cli containerd.io
 	# might need to reboot here
-	# change 1.28.5 to latest version
+	# change 1.29.2 to latest version
 	curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 	chmod +x /usr/local/bin/docker-compose
 }
