c989b1c3dd89c2f77f66d465a3fc803fcb117b00
NoDesireMonk <chinglish@gmail.com>
2022-10-20 11:52:38 -0500

    update docker installation

--- vdocker.sh
+++ vdocker.sh
@@ -462,17 +462,12 @@ function install_certbot {
 }
 
 function install_docker {
-	apt-get remove docker docker-engine docker.io containerd runc
-	apt update && apt -y install apt-transport-https ca-certificates curl gnupg lsb-release
-	curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-	# old method ==> curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-	echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
-	# old method ==> add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-	apt update && apt -y install docker-ce docker-ce-cli containerd.io
-	# might need to reboot here
-	# change 1.29.2 to latest version
-	curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-	chmod +x /usr/local/bin/docker-compose
+	apt purge docker docker-engine docker.io containerd runc
+	apt update && apt -y install ca-certificates curl gnupg lsb-release
+	mkdir -p /etc/apt/keyrings
+	curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+	apt update && apt -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 }
 
 function install_backport {
